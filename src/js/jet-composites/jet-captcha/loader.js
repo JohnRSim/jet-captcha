fc2ffe3871f600d94601cc76277f9dbd48fbf0ba
John Sim <jsim@fishbowlsolutions.com>
2019-08-13 21:10:15 +0100

    update licenses

--- src/js/jet-composites/jet-captcha/loader.js
+++ src/js/jet-composites/jet-captcha/loader.js
@@ -1,6 +1,6 @@
 /**
-  Copyright (c) 2015, 2019, Oracle and/or its affiliates.
-  The Universal Permissive License (UPL), Version 1.0
+  Copyright (c) 2017, 2019, JSEcoin Ltd.
+  GNU GENERAL PUBLIC LICENSE Version 3
 */
 define(['ojs/ojcomposite', 'text!./jet-captcha-view.html', './jet-captcha-viewModel', 'text!./component.json', 'css!./jet-captcha-styles'],
   function(Composite, view, viewModel, metadata) {
